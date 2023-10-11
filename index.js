98b70fa8a0813ad2493db0d71f8bce160638aa52
Fabrizio Pignatelli <fabriziopignatelli26@gmail.com>
2023-10-11 16:01:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+    
+    const newTask = document.getElementById("newTask"); //bottone
+    const main = document.getElementsByTagName("main")[0];
+        
+        // console.log(value);
+        const div = document.createElement("div");
+        div.className = "list";
+
+    const form = document.getElementById("addTask");
+    form.onsubmit = function(e){
+        e.preventDefault();
+        console.log("nuovo task");
+        const taskInput = document.getElementById("task"); //input box
+        // console.dir(taskInput);
+        const valore = taskInput.value;
+        const newButton = document.createElement("button");
+        newButton.id="deleteTask";
+        newButton.innerText="Cancella questa task";
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+
+        newButton.onclick = function (e) {
+            const clicked = e.target;
+            clicked.parentNode.remove();
+        }
+
+        li.innerText = valore;
+        li.appendChild(newButton);
+        ul.appendChild(li);
+        div.appendChild(ul);
+        main.appendChild(div);
+        taskInput.value = "";
+
+        li.addEventListener("click", () => {
+            li.style.textDecoration= "line-through";
+
+
+        })
+    }
+
+    // newTask.addEventListener("click", (e) => {
+    //     e.preventDefault();
+    //     console.log("nuovo task");
+    //     const taskInput = document.getElementById("task"); //input box
+    //     // console.dir(taskInput);
+    //     const valore = taskInput.value;
+    //     const ul = document.createElement("ul");
+    //     const li = document.createElement("li");
+
+    //     li.innerText = valore;
+
+    //     ul.appendChild(li);
+    //     div.appendChild(ul);
+    //     main.appendChild(div);
+
+    //     li.addEventListener("click", () => {
+    //         li.style.textDecoration= "line-through";
+    //     })
+
+
+    // })
